cf8dec4375b3496af0f49d9979cf997d497146f2
Samuel Banya <sbanya@fastmail.com>
2022-03-28 19:16:06 -0400

    Adding functions additions as per assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat() {
+    let moreCats = [...cats, "Broom"];
+    
+    return moreCats;
+}
+
+function prependCat() {
+    let prependCats = ["Arnold", ...cats];
+
+    return prependCats;
+}
+
+function removeLastCat() {
+    let removeLastCats = cats.slice(0, -1);
+
+    return removeLastCats;
+}
+
+function removeFirstCat() {
+    let removeFirstCats = cats.slice(1, cats.length);
+
+    return removeFirstCats;
+}
